5a58d7a0629df2e66147f0c29365091dada81ee4
anwarhakim31 <anwarhakim31@gmail.com>
2024-12-06 00:08:07 +0700

    progressive web app

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  ...nextConfig,
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { getServerSession } from "next-auth";
@@ -10,12 +9,52 @@ import QueryClientProvider from "@/components/Providers/QueryClientProvider";
 import { Toaster } from "@/components/ui/sonner";
 import ButtonScrollTop from "../components/ui/button-to-top";
 
+import type { Metadata, Viewport } from "next";
+
+const APP_NAME = "Pending Finance App";
+const APP_DEFAULT_TITLE = "Pending";
+const APP_TITLE_TEMPLATE = "%s - Pending Finance App";
+const APP_DESCRIPTION =
+  "Record your income and Recepint with Pending Finance App";
+
 export const metadata: Metadata = {
-  title: "Pending Finance",
-  description: "Record your income and receipts with Pending Finance",
-  icons: {
-    icon: "/coins.ico",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
   },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 const inter = Inter({
